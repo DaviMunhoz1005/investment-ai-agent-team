c86f73d65acf98ece6b8c4869db7ddf6c6a1c475
Davi Munhoz <davimunhoz1005@gmail.com>
2025-08-17 14:18:09 -0300

    feat(team-agents): create a team agent with search and financial agent

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+from agno.models.google import Gemini
+from agno.team import Team
+
 from agents.financial_agent import create_financial_agent
 from agents.search_agent import create_search_agent
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 financialAgent = create_financial_agent()
+
 searchAgent = create_search_agent()
+
+financialTeam = Team(
+    name="Financial Team",
+    model=Gemini(id="gemini-2.0-flash", api_key=os.getenv("GOOGLE_API_KEY")),
+    members=[financialAgent, searchAgent],
+    instructions="Present results in an ASCII table with columns: Stock Name | Current Price | Recommendation | Key Reason",
+    mode="coordinate",
+    markdown=True,
+    show_members_responses=True,
+    enable_agentic_context=True,
+    success_criteria="The team shows a complete table with stock name, price, whether it is recommended to buy or hold "
+                     "and the key reason for buying or holding",
+)
